f39f73a8635fd947f465931cb16bded890bdc74e
Mikabrytu <mikabrytu@gmail.com>
2018-03-09 01:06:59 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Levo Pão',
+      theme: new ThemeData(primarySwatch: Colors.deepPurple[800]),
+      home: new MyHomePage(title: 'Flutter Demo Home Page')
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text(widget.title)),
+      body: new Center(child: new Text("Levo Pão"))
+    );
+  }
+}
